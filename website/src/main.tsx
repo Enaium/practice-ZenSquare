4c2a722c21a08804be1df27e1445b84de425c8ca
Enaium <wyy-666@hotmail.com>
2023-05-02 13:09:34 +0800

    Update main.tsx

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -1,8 +1,13 @@
 import { createApp } from "vue"
-import "@/style.css"
+import "bootstrap/scss/bootstrap.scss"
 import App from "@/App"
-import index from "@/router"
+import router from "@/router"
+import { createPinia } from "pinia"
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate"
+import i18n from "@/i18n"
 
-createApp(<App />)
-  .use(index)
-  .mount("#app")
+const app = createApp(<App />)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(i18n).use(pinia).use(router)
+app.mount("#app")
